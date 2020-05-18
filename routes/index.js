861b8d2cd0ffd16cb96b1ea5cf63aedb755ced2a
Shubham Saha <saha.helinski@gmail.com>
2020-05-18 14:25:05 +0530

    add user sign-up

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,27 @@ var User = require('../models/user');
 // Auth Routes //
 /////////////////
 
-// Register ROUTE - for registering a new user
+// REGISTER ROUTE - Form for registering a new user
 router.get('/register', function(req, res) {
   res.render('register');
+});
+
+// CREATE ROUTE - Register a new user in the DB
+router.post('/register', function(req, res) {
+  // Set username
+  var newUser = new User({username: req.body.username});
+  // Register user using Passport middleware
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      // If registration is unsuccessful render the form again
+      console.log(err);
+      return res.redirect('register');
+    }
+    // If registration is successful redirect to 'blogs' page
+    passport.authenticate('local')(req, res, function() {
+      res.redirect('/blogs');
+    });
+  });
 });
 
 module.exports = router;
